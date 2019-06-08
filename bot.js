be9cab7084c0bc39d513954f2e92dc95bd4fb3fc
Nox <40596839+SheetSqueze@users.noreply.github.com>
2019-06-09 00:49:04 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on('08ui877uui86yy', () => {
+
+console.log('I am ready!');
+
+});
+
+
+
+client.on('-update', message => {
+
+if (message.content === '-update') {
+
+message.channel.send({embed: {
+
+color: 3447003,
+
+description: "Test!"
+
+});
+
+client.login(process.env. NTg3MDI0MDgwOTg1NjUzMjcy.XPwsRQ.58qnL22bYsdkZY4RUc3fPKjPjWg );
